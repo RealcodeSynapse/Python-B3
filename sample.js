52a244c2df99447db916a5dbf887cb675b4facdc
RealcodeSynapse <realcodesynapsekmm@gmail.com>
2025-05-17 12:01:51 +0530

    Add files via upload
    commit

---
+++ sample.js
@@ -0,0 +1,128 @@
+// let a = 20;
+// let b = 10;
+// let result = a + b;
+// console.log("result is :" + result);
+
+
+// var a = 3;
+
+// var b = 10;
+
+// console.log("a value is : " + a + " \n " + "b value is :" + b);
+
+// a=19;
+
+// console.log(a);
+
+
+// var a=20;
+
+// console.log(a);
+
+// let a=3;
+// a=19;
+// console.log(a);
+
+// let a=20;
+// console.log(a);
+// const a=3;
+// console.log(a);
+// a=19;
+// console.log(a);
+
+// const a =20 ;
+// console.log(a);
+
+
+// var a=30;
+// a++;  //a=a+1  ->a= 30+1
+// console.log(a);
+
+// let b=20;
+// b++;
+// console.log(b);
+
+
+// const c=40;
+// console.log(c);
+
+// c++;  //c=c+1  -> 40+1
+// console.log(c);
+// {
+//     var a=30;
+// a++;  //a=a+1  ->a= 30+1
+// console.log(a);
+
+// }
+
+// console.log(a);
+
+// {
+//    let b=20;
+// b++;
+// console.log(b);
+ 
+// }
+// console.log(b);
+// let e=89;
+// {
+//     const c=40;
+// console.log(c);
+// let d=30;
+// console.log(d);
+
+// }
+// console.log(e);
+
+
+// let names = "Vijaya";
+// console.log("My name is :" + names);
+
+// let msg ="Hello"
+// msg += " Vijaya!!";
+// console.log(msg);
+
+
+// let firstname="Vijaya";
+// let lastname="p";
+
+// let fullname = `${firstname} ${lastname}`;
+
+// console.log(fullname);
+
+// let a=0;
+// let b=22.7;
+// let c=7878847348.00;
+// let d=535332.999;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// let age=17;
+// if(age>=18){
+//     console.log("he is elible for voting"); 
+// }
+// else{
+//     console.log("he not is elible for voting"); 
+// }
+
+
+// let g="ff";
+
+
+// console.log(g);
+
+
+
+
+// let arr=[5,6,7,8,9,1,2,3,4];
+// console.log(arr[5]);
+
+// arr[4] =0;
+// console.log(arr[4]);
+
+// console.log(arr);
+
